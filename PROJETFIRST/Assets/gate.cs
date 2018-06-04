057b7590440f5d820c2d8652d8c0696806c679d5
Unknown <bibibenji@laposte.net>
2018-06-04 16:46:45 +0200

    import de l'asset "temple" et ajout de la porte

---
+++ PROJETFIRST/Assets/gate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gate : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (player.mouton >= 3)
+		{
+				openGate();
+		}
+	}
+
+	void openGate()	{
+		this.GetComponent.transform.position.x = 5.5;
+	}
+
+}
