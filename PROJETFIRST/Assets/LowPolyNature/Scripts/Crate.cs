49ce03078005933b68eb4c84500dcbf365a37494
ProjetFirst <espritfablab@gmail.com>
2018-06-05 16:40:09 +0200

    NOUVELLE MAP + RESOLUTION DE BUG

--- PROJETFIRST/Assets/LowPolyNature/Scripts/Crate.cs
+++ PROJETFIRST/Assets/LowPolyNature/Scripts/Crate.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Crate : InteractableItemBase {
 
     private bool mIsOpen = false;
+    public GameObject torche;
 
     public override void OnInteract()
     {
@@ -14,5 +15,11 @@ public class Crate : InteractableItemBase {
         InteractText += mIsOpen ? "to close" : "to open";
 
         GetComponent<Animator>().SetBool("open", mIsOpen);
+          torche.GetComponent<BoxCollider>().enabled = !torche.GetComponent<BoxCollider>().enabled;
+
+          GameObject [] t = GameObject.Find("torch_A").GetComponentsInChildren<GameObject>();
+          t[0].active = !t[0].active;
+          //GameObject.Find("torch_Particle").active = !GameObject.Find("torch_Particle").active;
+
     }
 }
